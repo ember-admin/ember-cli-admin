a0d490d0e36c1dd35d08423b8cc6974ae8352ac4
Opak Alex <opak.alexandr@gmail.com>
2013-08-15 13:10:13 +0300

    close #127

--- development/app/controllers/users_controller.rb
+++ development/app/controllers/users_controller.rb
@@ -1,12 +1,7 @@
 class UsersController < ActionController::Base
 
   def index
-    page = (params[:page] || 1).to_i
-    per_page = (params[:per_page] || 25).to_i
-    count = page * per_page
-    start = 0
-    start = (page - 1) * per_page if page > 1
-    render json: {users: User.order('id desc').all()[start...count]}
+    render json: {users: User.paginate(page: params[:page], per_page: params[:per_page]).order('id desc')}
   end
 
   def destroy
