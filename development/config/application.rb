a4ed27008d0e440a1ed18e649ede1a11653716a2
Opak Alex <opak.alexandr@gmail.com>
2013-08-22 20:27:06 +0300

    add as assets path #140

--- development/config/application.rb
+++ development/config/application.rb
@@ -20,6 +20,8 @@ module EmberRailsAdmin
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
     config.generators do |g|
       g.test_framework :rspec
       g.template_engine :slim
