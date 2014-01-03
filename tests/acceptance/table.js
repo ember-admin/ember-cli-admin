32ed189d0e3763155581416c08837f393b6d8b61
Opak Alex <opak.alexandr@gmail.com>
2014-01-03 15:47:30 +0200

    add test files

--- tests/acceptance/table.js
+++ tests/acceptance/table.js
@@ -7,8 +7,19 @@ module('Table tests: Batches', {
   }
 });
 
-//test('Table should have actions and itams', function(){
-//  visit('persons').then(function(){
-//
-//  });
-//});
+test('Table should have actions and items', function(){
+  expect(3);
+  visit('persons').then(function(){
+    var count = find("table .ember-checkbox").size() - 1;
+    var editActions = find(".glyphicon.glyphicon-pencil").size();
+    equal(editActions, count);
+
+    var infoActions = find(".glyphicon.glyphicon-info-sign").size();
+    equal(infoActions, count);
+
+    var deleteActions = find(".glyphicon.glyphicon-trash").size();
+    equal(deleteActions, count);
+  });
+});
+
+//Todo
