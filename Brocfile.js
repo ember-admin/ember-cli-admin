fd7db4b5062c1b2a1c55b25cf7b93ecd09aa54c5
ValeriaLeshchenko <atargatis.the.one@gmail.com>
2014-10-14 16:55:46 +0300

    Fix brocfile to create vendor.css correctly; update ember-cli-map

--- Brocfile.js
+++ Brocfile.js
@@ -9,20 +9,6 @@ app.import('bower_components/ember-forms/dist/globals/main.js');
 app.import('bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js');
 
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
-
-// module.exports = app.toTree();
 
 // Put the bootstrap fonts in the place that the bootstrap css expects to find them.
 var pickFiles = require('broccoli-static-compiler');
@@ -30,11 +16,9 @@ var bootstrapFonts = pickFiles('bower_components/bootstrap-sass-official/assets/
     srcDir: '/',
     destDir: '/assets/bootstrap'
 });
-
-var compileSass = require('broccoli-sass');
-var adminCss   = compileSass(['app/styles'], 'app.scss',  'assets/vendor.css');
-
+var mergeTrees = require('broccoli-merge-trees');
+app.import('vendor/app.css');
 
 // Merge the bootstrapFonts with the ember app tree
-var mergeTrees = require('broccoli-merge-trees');
-module.exports = mergeTrees([app.toTree(),bootstrapFonts, adminCss], { overwrite: true });
+
+module.exports = mergeTrees([app.toTree(), bootstrapFonts]);
