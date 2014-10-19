e774c3ad52957d4335845904ae30744b75315896
Valeria <atargatis.the.one@gmail.com>
2014-10-19 21:00:46 +0300

    Add ember-cli-admin generator

---
+++ blueprints/ember-cli-admin/index.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var path = require('path');
+var jsonFile, packageJsonPath, properties, self;
+module.exports = {
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
+  afterInstall: function() {
+   packageJsonPath = path.join(this.project.root, 'package.json');
+   jsonFile = require(packageJsonPath);
+    properties = {
+      "ember-cli-map": "^0.2.0",
+      "broccoli-emblem-compiler": "^0.3.18",
+      "broccoli-merge-trees": "0.1.4",
+      "broccoli-sass": "^0.2.2",
+      "broccoli-static-compiler": "0.1.4",
+      "ember-cli-coffeescript": "^0.1.0",
+      "broccoli-coffee": "^0.1.1"
+    };
+    Object.keys(properties).forEach(function (key) {
+     jsonFile.devDependencies[key] = properties[key];
+    });
+   fs.writeFileSync(packageJsonPath, JSON.stringify(jsonFile, null, 2));
+
+   bowerFilePath = path.join(this.project.root, 'bower.json');
+   bowerFile = require(bowerFilePath);
+   bowerFile.dependencies["bootstrap-sass-official"] = "3.2.0";
+   return fs.writeFileSync(bowerFilePath, JSON.stringify(bowerFile, null, 2));
+  }
+};
