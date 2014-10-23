f954cd92010759ceb0cf8bd881f545fee921dccf
ValeriaLeshchenko <atargatis.the.one@gmail.com>
2014-10-23 16:52:47 +0300

    Arrange tests in separate files

---
+++ tests/acceptance/smoke-test.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+
+var App, users;
+
+module('Acceptance: Smoke Test', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('root displays dashboard', function() {
+  expect(1);
+  visit('/');
+  equal(find("h1:contains('Dashboard')").length, 1);
+});
+
+
+test('navbar contains links to dashboard and resources', function() {
+  expect(2);
+  visit('/');
+  equal(find("a:contains('Users')").length, 1);
+  equal(find("a:contains('Dashboard')").length, 1);
+});
