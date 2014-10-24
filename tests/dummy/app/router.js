387cde8da48a5a769bcc783e8e828b660a98de6f
ValeriaLeshchenko <atargatis.the.one@gmail.com>
2014-10-24 16:06:04 +0300

    Upgrade ember-cli to 0.1.2

---
+++ tests/dummy/app/router.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import MetaRoute from 'ember-cli-admin/dsl/meta-route';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route("dashboard", {path: "/"});
+});
+
+MetaRoute.map(Router, function() {
+  this.resources("users");
+  this.resources("cars");
+  this.resources("user-categories");
+});
+
+export default Router;
