bbace901c1a47f0c1d7a09e61c09add2a3293e79
Valeria <atargatis.the.one@gmail.com>
2014-10-28 18:26:29 +0200

    Edit generator; add tree view test

--- blueprints/ember-cli-admin/index.js
+++ blueprints/ember-cli-admin/index.js
@@ -18,23 +18,24 @@ module.exports = {
   afterInstall: function() {
    packageJsonPath = path.join(this.project.root, 'package.json');
    jsonFile = require(packageJsonPath);
-    properties = {
-      "ember-cli-map": "^0.2.2",
-      "broccoli-merge-trees": "0.1.4",
-      "broccoli-sass": "^0.2.2",
-      "broccoli-static-compiler": "0.1.4",
-      "ember-cli-coffeescript": "^0.1.0",
-      "broccoli-coffee": "^0.1.1"
-    };
-    Object.keys(properties).forEach(function (key) {
+   properties = {
+     "ember-cli-map": "0.2.2",
+     "broccoli-merge-trees": "0.1.4",
+     "broccoli-sass": "^0.2.2",
+     "broccoli-static-compiler": "0.1.4",
+     "ember-cli-coffeescript": "^0.1.0",
+     "broccoli-coffee": "^0.1.1"
+   };
+   Object.keys(properties).forEach(function (key) {
      jsonFile.devDependencies[key] = properties[key];
-    });
+   });
    fs.writeFileSync(packageJsonPath, JSON.stringify(jsonFile, null, 2));
 
    bowerFilePath = path.join(this.project.root, 'bower.json');
    bowerFile = require(bowerFilePath);
    bowerFile.dependencies["bootstrap-sass-official"] = "3.2.0";
    bowerFile.dependencies["typeahead.js"] = "twitter/typeahead.js#0.10.5";
+   bowerFile.dependencies["jquery-ui-sortable"] =  "*";
    return fs.writeFileSync(bowerFilePath, JSON.stringify(bowerFile, null, 2));
   }
 };
---
+++ tests/acceptance/tree-view-test.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+import Pretender from 'pretender';
+
+var App, server, catalogues;
+
+module('Acceptance: Tree View Test', {
+  setup: function() {
+    App = startApp();
+    server = new Pretender(function() {
+      this.get('/api/catalogues', function(request) {
+        catalogues = [{id: 1, name: 'Tree #1', parent_id: null, catalogue_ids: [2,3]},
+                          {id: 2, name: 'Tree #2', parent_id: 1, catalogue_ids: [4]},
+                          {id: 3, name: 'Tree #3', parent_id: 1, catalogue_ids: [5]},
+                          {id: 4, name: 'Tree #4', parent_id: 2, catalogue_ids: []},
+                          {id: 5, name: 'Tree #5', parent_id: 3, catalogue_ids: []}];
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({catalogues: catalogues})];
+      });
+    });
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+    server.shutdown();
+  }
+});
+
+test('model tree is displayed', function() {
+  expect(1);
+
+  visit('/catalogues');
+
+  andThen(function() {
+    equal(find('.sortable_tree li:first:contains("1") .sortable_tree li:first:contains("2") ' +
+        '.sortable_tree li:first:contains("4")').length, 1);
+  });
+
+});
