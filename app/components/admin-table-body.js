0a7cf2a1f4a909cee66ecd0d657abae2ad81dcd0
Drew Nichols <drew@liftforward.com>
2014-12-24 10:13:47 -0500

    removing console.log messages and cleanup

--- app/components/admin-table-body.js
+++ app/components/admin-table-body.js
@@ -4,7 +4,6 @@ export default Ember.Component.extend({
   tagName: "tbody",
   actions: {
     adminAction: function(adminActionName, model) {
-      console.log('admin-table-body.adminAction: ', adminActionName, model);
       this.sendAction('adminAction', adminActionName, model);
     }
   }
--- app/components/admin-tr-list.js
+++ app/components/admin-tr-list.js
@@ -2,10 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: "tr",
-  classNames: "",
   actions: {
     adminAction: function(adminActionName, model) {
-      console.log('admin-tr.adminAction: ', adminActionName, model);
       this.sendAction('adminAction', adminActionName, model);
     }
   }
