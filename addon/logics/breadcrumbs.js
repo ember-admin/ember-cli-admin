df4172395bb0e911d04c07415d1ee1c10d5ae3d5
Opak Alex <opak.alexandr@gmail.com>
2014-12-25 16:34:22 +0200

    refactor breadcrumbs

--- addon/logics/breadcrumbs.js
+++ addon/logics/breadcrumbs.js
@@ -5,43 +5,29 @@ var breadcrumbs;
 breadcrumbs = Ember.Object.extend();
 
 breadcrumbs.reopenClass({
-  setup: function(action, controller, model, breadcrumbs_controller) {
+  setup: function(action, controller, model, breadcrumbsController) {
     var content, name, obj;
     content = [];
-    obj = Ember.Object.create({
-      name: "dashboard",
-      url: this._url("dashboard"),
-      "class": "first",
-      active: false
-    });
+    content.pushObject(this._dashboard());
+    obj = this._controllerLink(controller.get('_name'));
     content.pushObject(obj);
-    obj = Ember.Object.create({
-      name: controller.get('_name'),
-      url: this._url(controller.get('_name')),
-      "class": "active",
-      active: true
-    });
     if (action && model) {
       obj.set('class', "");
       obj.set('active', false);
-      content.pushObject(obj);
       name = model.get('id') || action;
-      obj = Ember.Object.create({
-        name: name,
-        "class": "active",
-        active: true
-      });
-      content.pushObject(obj);
-    } else {
-      content.pushObject(obj);
+      content.pushObject(this._controllerActionLink(name));
     }
-    breadcrumbs_controller.set('content', content);
+    breadcrumbsController.set('content', content);
     return this._actions(action, controller);
   },
   _url: function(url) {
     return url;
   },
   _actions: function(action, controller) {
+    if(!controller.get('itemActions')){
+      return;
+    }
+
     var actions;
     actions = [];
     switch (action) {
@@ -58,20 +44,51 @@ breadcrumbs.reopenClass({
       default:
         actions.push(this._createAction());
     }
+    var controllerItemActions = controller.get('itemActions').map(function(item){return item.title;});
+    actions = actions.filter(function(action){
+      return controllerItemActions.indexOf(action) >= 0;
+    });
     return controller.set("__breadcrumbsActionsArray", actions);
   },
   _createAction: function() {
-    return "new";
+    return "New";
   },
   _editAction: function() {
-    return "edit";
+    return "Edit";
   },
   _destroyAction: function() {
-    return "destroy";
+    return "Destroy";
   },
   _showAction: function() {
-    return "show";
+    return "Show";
+  },
+
+  _dashboard: function(){
+    return Ember.Object.create({
+      name: "dashboard",
+      url: this._url("dashboard"),
+      "class": "first",
+      active: false
+    });
+  },
+
+  _controllerLink: function(name){
+    return Ember.Object.create({
+      name: name,
+      url: name,
+      "class": "active",
+      active: true
+    });
+  },
+
+  _controllerActionLink: function(name){
+    return  Ember.Object.create({
+      name: name,
+      "class": "active",
+      active: true
+    });
   }
+
 });
 
 export default breadcrumbs;
