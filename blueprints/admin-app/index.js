7f14f202708fc49dd6f5696720a4ff3e5f26b601
Igor Kur <kur91@mail.ru>
2015-01-17 18:35:29 +0200

    app scaffold

--- blueprints/admin-app/index.js
+++ blueprints/admin-app/index.js
@@ -1,8 +1,10 @@
 module.exports = {
-    description: 'Generates an acceptance test for a feature.'
-      beforeInstall: function() {
-        this.insertIntoFile('app/app.js',
-                             "import AdminResolver from 'ember-cli-admin/admin-resolver';",
-                                              {after:"import config from './config/environment';"});
-      }
+  description: 'Generates an acceptance test for a feature.',
+  beforeInstall: function() {
+    this.insertIntoFile('app/app.js',
+      "import AdminResolver from 'ember-cli-admin/admin-resolver';", {
+        after: "import config from './config/environment';" + "\n"
+      });
+
+  }
 };
