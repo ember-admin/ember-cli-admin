9c7b774fecc6c7dc8dcd9ebee6f2366d10bc8f47
Igor Kur <kur91@mail.ru>
2015-01-18 23:35:54 +0200

    brocfile

--- blueprints/admin-app/index.js
+++ blueprints/admin-app/index.js
@@ -5,36 +5,45 @@ module.exports = {
   description: 'Generates an acceptance test for a feature.',
   beforeInstall: function(options) {
     var process = processTextContent.bind(this);
-    renameFile.bind(this)('app/styles/app.css', 'app/styles/app.scss');
-    process('app/app.js',
+    // renameFile.bind(this)('app/styles/app.css', 'app/styles/app.scss');
+    // process('app/app.js',
+    //   {
+    //     replace: {
+    //       'ember/resolver': "ember-cli-admin/admin-resolver",
+    //       'Resolver: Resolver': 'Resolver: AdminResolver',
+    //       'import Resolver': 'import AdminResolver'
+    //     }
+    //   });
+    // process('app/templates/application.hbs',
+    //   {
+    //     replace: {
+    //       '<h2 id="title">Welcome to Ember.js</h2>\n\n{{outlet}}': "{{partial 'admin/index'}}"
+    //     }
+    //   });
+    // process('app/router.js',
+    //   {
+    //     replace: {
+    //       "Router.map(function() {": "Router.map(function() {\n\treturn this.route(\"dashboard\", {path: \"/\"});"
+    //     },
+    //     insert: {
+    //       "import config from './config/environment';": "\nimport MetaRoute from 'ember-cli-admin/dsl/meta-route';",
+    //       "Router.map(function() {\n\treturn this.route(\"dashboard\", {path: \"/\"});\n});\n": "\nMetaRoute.map(Router, function() {\n});"
+    //     }
+    //   });
+    // var appName = "\n\t\t\tappName: '" + options.entity.name + "',";
+    // process('config/environment.js',
+    //   {
+    //     insert: {
+    //       "EmberENV: {": appName
+    //     }
+    //   });
+    process('Brocfile.js',
       {
-        replace: {
-          'ember/resolver': "ember-cli-admin/admin-resolver",
-          'Resolver: Resolver': 'Resolver: AdminResolver',
-          'import Resolver': 'import AdminResolver'
-        }
-      });
-    process('app/templates/application.hbs',
-      {
-        replace: {
-          '<h2 id="title">Welcome to Ember.js</h2>\n\n{{outlet}}': "{{partial 'admin/index'}}"
-        }
-      });
-    process('app/router.js',
-      {
-        replace: {
-          "Router.map(function() {": "Router.map(function() {\n\treturn this.route(\"dashboard\", {path: \"/\"});"
+        insert: {
+          "EmberApp();": "\nvar pickFiles = require('broccoli-static-compiler');\nvar bootstrapFonts = pickFiles('bower_components/bootstrap-sass-official/assets/fonts/bootstrap', {\n\tsrcDir: '/',\n\tdestDir: '/assets/bootstrap'\n});\nvar mergeTrees = require('broccoli-merge-trees');"
         },
-        insert: {
-          "import config from './config/environment';": "\nimport MetaRoute from 'ember-cli-admin/dsl/meta-route';",
-          "Router.map(function() {\n\treturn this.route(\"dashboard\", {path: \"/\"});\n});\n": "\nMetaRoute.map(Router, function() {\n});"
-        }
-      });
-    var appName = "\n\t\t\tappName: '" + options.entity.name + "',";
-    process('config/environment.js',
-      {
-        insert: {
-          "EmberENV: {": appName
+        replace: {
+          "module.exports = app.toTree();": "module.exports = mergeTrees([app.toTree(), bootstrapFonts]);"
         }
       });
 
