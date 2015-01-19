3892cb68afa4ddf8f610f0741a92779d5a70d66a
Igor Kur <kur91@mail.ru>
2015-01-19 12:26:10 +0200

    add adapter

--- blueprints/ember-cli-admin/index.js
+++ blueprints/ember-cli-admin/index.js
@@ -3,21 +3,21 @@ var path = require('path');
 var renameFile = require('ember-cli-admin/lib/proccess-text-content').renameFile;
 
 module.exports = {
-
+  normalizeEntityName: function() {},
   afterInstall: function() {
     renameFile.bind(this)('app/styles/app.css', 'app/styles/app.scss');
-    this.addPackagesToProject([
+    this.addBowerPackagesToProject([
+      {name: "bootstrap-sass-official", target: "3.2.0"},
+      {name: "typeahead.js", target: "0.10.5"},
+      {name: "jquery-ui-sortable", target: "*"}
+    ]);
+    return this.addPackagesToProject([
         {name: "ember-cli-map", target: "0.2.2"},
         {name: "broccoli-merge-trees", target: "0.1.4"},
         {name: "broccoli-sass", target: "^0.2.2"},
         {name: "broccoli-static-compiler", target: "0.1.4"},
         {name: "ember-cli-coffeescript", target: "^0.1.0"},
         {name: "broccoli-coffee", target: "^0.1.1"}
-    ]);
-    return this.addBowerPackagesToProject([
-      {name: "bootstrap-sass-official", target: "3.2.0"},
-      {name: "typeahead.js", target: "twitter/typeahead.js#0.10.5"},
-      {name: "jquery-ui-sortable", target: "*"}
     ]);
   }
 };
