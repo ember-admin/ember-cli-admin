d574cc5b03ba5a81bd8ad44eb4243469240a7a58
Igor Kur <kur91@mail.ru>
2015-02-07 18:24:26 +0200

    fix form

--- app/components/admin-input.js
+++ app/components/admin-input.js
@@ -3,27 +3,31 @@ var adminInput;
 
 adminInput = Ember.Component.extend({
     type: function(){
-        return Ember.typeOf(this.get('model').get(this.get('name')));
-    }.property('name', 'model'),
+        var transformedAttributes = Ember.get(this.get('model.constructor'), 'transformedAttributes')
+        if(Ember.isEmpty(this.get('value'))){
+            return transformedAttributes.get(this.get('name'));
+        }
+        return Ember.typeOf(this.get('value'));
+    }.property('value'),
 
     isString: Ember.computed.equal('type', 'string'),
 
     isEmail: function(){
-        if(this.get('isString') && this.get('value').match(/.+@\w+\.\w+/)){
+        if(this.get('isString') && this.get('value') && this.get('value').match(/.+@\w+\.\w+/)){
             this.set('isString', false);
             return true;
         }
         return false;
     }.property('isString'),
     isColor: function(){
-        if(this.get('isString') && this.get('value').match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/)){
+        if(this.get('isString') && this.get('value') && this.get('value').match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/)){
             this.set('isString', false);
             return true;
         }
         return false;
     }.property('isString'),
     isText: function(){
-        if(this.get('isString') && this.get('value').length > 50){
+        if(this.get('isString') && this.get('value') && this.get('value').length > 50){
             this.set('isString', false);
             return true;
         }
