61420964acba0dc6075ee51e8a994c86289846e0
Igor Kur <kur91@mail.ru>
2015-02-18 01:17:48 +0200

    [#366] closed

--- blueprints/table-view-controller/index.js
+++ blueprints/table-view-controller/index.js
@@ -1,3 +1,21 @@
+var path = require('path');
+
 module.exports = {
-        description: 'Generates tableview controller for specified resource'
+    description: 'Generates tableview controller for specified resource',
+    fileMapTokens: function() {
+        return {
+            __path__: function(options) {
+                if (options.pod) {
+                    return path.join(options.podPath, options.dasherizedModuleName);
+                }
+                return 'controllers';
+            },
+            __name__: function(options) {
+                if (options.pod) {
+                    return "controller";
+                }
+                return options.dasherizedModuleName;
+            }
+        };
+    }
 };
--- blueprints/table-view-resource/index.js
+++ blueprints/table-view-resource/index.js
@@ -1,5 +1,6 @@
 var inflection = require('ember-cli/node_modules/inflection');
 var processTextContent = require('ember-cli-admin/lib/proccess-text-content').processTextContent;
+var path = require('path');
 
 module.exports = {
     description: 'Generates tableview resource',
@@ -7,10 +8,28 @@ module.exports = {
     fileMapTokens: function() {
         return {
             __modelName__: function(options) {
+                if (options.pod) {
+                    return "model";
+                }
                 return inflection.singularize(options.dasherizedModuleName);
             },
             __controllerName__: function(options){
+                if (options.pod) {
+                    return "controller";
+                }
                 return inflection.pluralize(options.dasherizedModuleName);
+            },
+            __controllerPath__: function(options) {
+                if (options.pod) {
+                    return path.join(options.podPath,  inflection.pluralize(options.dasherizedModuleName));
+                }
+                return 'controllers';
+            },
+            __modelPath__: function(options) {
+                if (options.pod) {
+                    return path.join(options.podPath, inflection.singularize(options.dasherizedModuleName));
+                }
+                return 'models';
             }
         };
     },
