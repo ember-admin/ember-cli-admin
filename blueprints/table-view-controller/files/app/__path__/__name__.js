99c508139eab85365020d5542e8c551172201f6c
iggor_kurr <kurinnoyi@gmail.com>
2015-04-01 11:55:55 +0300

    fix deprecations

--- blueprints/table-view-controller/files/app/__path__/__name__.js
+++ blueprints/table-view-controller/files/app/__path__/__name__.js
@@ -1,4 +1,4 @@
 import Ember from 'ember';
 import TableViewController from 'ember-cli-admin/mixins/controllers/table-view';
 
-export default Ember.ObjectController.extend(TableViewController);
+export default Ember.Controller.extend(TableViewController);
--- blueprints/tree-view-resource/files/app/__controllerPath__/__controllerName__.js
+++ blueprints/tree-view-resource/files/app/__controllerPath__/__controllerName__.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import TreeViewController from 'ember-cli-admin/mixins/controllers/tree-view';
 
-export default Ember.ObjectController.extend(TreeViewController, {
+export default Ember.Controller.extend(TreeViewController, {
   formAttributes: ['name']
 });
