729b6443e57be0b946ca8d2cbe590f871c80caa2
iggor_kurr <kurinnoyi@gmail.com>
2015-07-17 00:48:34 +0300

    fix admin-select

--- addon/components/admin-select.js
+++ addon/components/admin-select.js
@@ -3,25 +3,28 @@ import layout from '../templates/components/admin-select';
 
 export default Ember.Component.extend({
   layout: layout,
+  tagName: 'select',
 
   content: [],
   selectedValue: null,
 
-  actions: {
-    change() {
-      const changeAction = this.get('action');
-      const selectedEl = this.$('select')[0];
-      let selectedIndex = selectedEl.selectedIndex;
-      if (this.get('prompt')){
-        selectedIndex = selectedIndex - 1;
-      }
-      const content = this.get('content');
-      const selectedValue = content[selectedIndex];
-      if (selectedValue){
+  change() {
+    this._change();
+  },
+
+  _change() {
+    const selectedEl = this.$()[0];
+    let selectedIndex = selectedEl.selectedIndex;
+    if (this.get('prompt')) {
+      selectedIndex = selectedIndex - 1;
+    }
+    const content = this.get('content');
+    const selectedValue = content.toArray()[selectedIndex];
+    if (selectedValue) {
+      if (this.get('action')) {
+        this.sendAction('action', selectedValue);
+      } else {
         this.set('selectedValue', selectedValue);
-        if (changeAction){
-          changeAction(selectedValue);
-        }
       }
     }
   }
