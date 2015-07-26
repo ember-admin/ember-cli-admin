e07ac291da4172dc451e34a3abe3ab0b674b2835
iggor_kurr <kurinnoyi@gmail.com>
2015-07-26 12:39:26 +0300

    update controller blueprints

--- blueprints/table-view-controller/index.js
+++ blueprints/table-view-controller/index.js
@@ -1,21 +1,6 @@
-var path = require('path');
+/*jshint node:true*/
 
 module.exports = {
   description: 'Generates tableview controller for specified resource',
-  fileMapTokens: function() {
-    return {
-      __path__: function(options) {
-        if (options.pod) {
-          return path.join(options.podPath, options.dasherizedModuleName);
-        }
-        return 'controllers';
-      },
-      __name__: function(options) {
-        if (options.pod) {
-          return "controller";
-        }
-        return options.dasherizedModuleName;
-      }
-    };
   }
 };
